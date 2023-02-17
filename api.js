d5e7d2554af040e4dca742f337621887f454ea6e
Luisazizzo <luisa0zizzo@gmail.com>
2023-02-17 18:45:13 +0100

    Modifiche sito

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 //const API_KEY = "81ba2e9858004b88b95155122233001";
-const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=81ba2e9858004b88b95155122233001&q=`;
+const BASE_URL = `https://api.weatherapi.com/v1/current.json?key=81ba2e9858004b88b95155122233001&q=`;
 
 const GET = async (city) => {
   const res = await fetch(`${BASE_URL}${city}`);
